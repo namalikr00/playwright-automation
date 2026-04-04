362990cdb0066a76bb36b99cecb41300013171b3
namalikr00 <knemalidinne@qcrh.com>
2026-04-04 14:54:02 -0500

    Create MfaPage.js

---
+++ pageObjects/gbank/MfaPage.js
@@ -0,0 +1,44 @@
+const { expect } = require('@playwright/test');
+export class MfaPage {
+    constructor(page) {
+        this.page = page;
+        this.quickTipContainer = page.locator("[class='quicktip-container']");
+        this.deliveryMethodTipText = page.locator('.tip-text');
+        this.selectTargetTitle = page.getByText('Please select a target:');
+        this.backButton = page.getByText('Back');
+        this.submitButton = page.getByText('Submit');
+        this.emailMeButton = page.locator('#login-inner > div.login-form.loginFormArea.pad-T-xl.pad-H-md').getByText('Email me:');
+        this.textMeButton = page.locator('#login-inner > div.login-form.loginFormArea.pad-T-xl.pad-H-md').getByText('Text me:');
+        this.callMeButton = page.locator('#login-inner > div.login-form.loginFormArea.pad-T-xl.pad-H-md').getByText('Call me:');
+        this.securityCodeInput = page.getByPlaceholder('Secure Access Code');
+        this.fdicBanner = page.locator("[class='quicktip-container']");
+        this.secureCodeLabel = page.getByText('Enter your Secure Access Code');
+        this.tokenInput = page.locator('#input-guid-1003');
+        this.microsoftSignInButton = page.locator('#c-shellmenu_custom_outline_signin_bhvr100_right');
+        this.microsofUserEmailIdButton = page.locator('.table-cell.text-left.content').first();
+        this.microsoftEmailIdInput = page.locator('#i0116');
+        this.microsoftNextButton = page.locator('#idSIButton9');
+        this.microsoftPasswordInput = page.locator('#i0118');
+        this.msSignInButton = page.locator('#idSIButton9')
+    }
+
+    async verifyMfaPageElements() {
+        await expect(this.quickTipContainer).toBeVisible();
+        await expect(this.deliveryMethodTipText).toBeVisible();
+        await expect(this.deliveryMethodTipText).toHaveText("Choose a delivery method you can readily access. Add code delivery options in Security Preferences under SETTINGS.");
+        await expect(this.selectTargetTitle).toBeVisible();
+        await expect(this.emailMeButton).toBeVisible();
+        await expect(this.textMeButton).toBeVisible();
+        await expect(this.callMeButton).toBeVisible();
+    }
+
+    async selectEmailAuthentication() {
+        await this.textMeButton.click();
+        await this.page.waitForLoadState('networkidle');
+        await expect(this.fdicBanner).toBeVisible();
+        await expect(this.secureCodeLabel).toBeVisible();
+        await expect(this.backButton).toBeVisible();
+        await expect(this.submitButton).toBeVisible();
+        await expect(this.tokenInput).toBeVisible();
+    }
+}
