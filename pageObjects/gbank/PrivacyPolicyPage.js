1f2189002761ed26c0698ab0843ece2bfb794988
namalikr00 <knemalidinne@qcrh.com>
2026-04-04 14:57:50 -0500

    Create PrivacyPolicyPage.js

---
+++ pageObjects/gbank/PrivacyPolicyPage.js
@@ -0,0 +1,32 @@
+import { expect } from '@playwright/test';
+
+export class PrivacyPolicyPage {
+    constructor(page) {
+        this.page = page;
+        this.pageTitle = this.page.getByRole('heading', { name: 'Privacy', exact: true });
+    }
+
+    // Define locators for elements on the new tab
+    get header() {
+        return this.page.locator('h1');
+    }
+
+    // Add other locators as needed
+    get someButton() {
+        return this.page.getByRole('button', { name: 'Submit' });
+    }
+
+    // Define methods to interact with the new tab
+    async verifyHeader(expectedText) {
+        await this.pageTitle.waitFor({ state: 'visible' });
+        await expect(this.pageTitle).toHaveText(expectedText);
+    }
+
+    async clickSomeButton() {
+        await this.someButton.click();
+    }
+
+    async verifyPageTitle(expectedPageTitle) {
+        await expect(this.page).toHaveTitle(expectedPageTitle);
+    }
+}
