2b8beef6d9ee40f7a14b018d6e747d2f151ea065
namalikr00 <knemalidinne@qcrh.com>
2026-04-04 14:42:26 -0500

    Create NoticeOfChangeLetterPage.js

---
+++ pageObjects/crbt/NoticeOfChangeLetterPage.js
@@ -0,0 +1,23 @@
+import { expect } from '@playwright/test';
+
+export class NoticeOfChangeLetterPage {
+    constructor(page) {
+        this.page = page;
+        this.pageTitle = this.page.getByRole('heading', { name: 'Notice of Important Changes', exact: true });
+    }
+
+    // Define locators for elements on the new tab
+    get header() {
+        return this.page.locator('h1');
+    }
+
+    // Define methods to interact with the new tab
+    async verifyHeader(expectedText) {
+        await this.pageTitle.waitFor({ state: 'visible' });
+        await expect(this.pageTitle).toHaveText(expectedText);
+    }
+
+    async verifyPageTitle(expectedPageTitle) {
+        await expect(this.page).toHaveTitle(expectedPageTitle);
+    }
+}
